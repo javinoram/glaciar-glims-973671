2917062e0e0e92efe87b87f8e5972ea27345a19c
jnorambu <javinorambl@gmail.com>
2025-04-28 20:23:57 -0300

    28/04/2025 Feat:Primera version calculo thar

---
+++ thar/thar.py
@@ -0,0 +1,22 @@
+
+def elevation_limit_altitude(t, ab, am):
+    return ab + t*(am - ab)
+
+
+#Constantes de thar segun Poter et al (1983)
+#indica valores ente 0.4 y 0.5 para glaciares limpios
+thar_constants = [0.4, 0.425, 0.45, 0.475, 0.5]
+
+
+#Pares de altura de diferentes puntos de la geomorfologia
+#del glaciar
+list_of_pairs = [(600, 900), (580, 900), (490, 900), (470, 900)]
+
+
+for (ab, am) in list_of_pairs:
+    print(f"Altura basal: {ab} - Altura cima: {am}")
+    for t in thar_constants:
+        ELA = elevation_limit_altitude(t, ab, am)
+        print(ELA)
+
+
